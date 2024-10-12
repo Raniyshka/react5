30e91df02653335301591fb21f0fc3e728e5affc
Raniyshka <raniya.good2@gmail.com>
2024-10-13 00:23:52 +0300

    практика 5 react

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+export const products = [
+    {
+        img: 'https://ir-3.ozone.ru/s3/multimedia-1-7/wc1000/7029624787.jpg',
+        name: 'product1',
+        price: 700
+    },
+    {
+        img: 'https://ir-3.ozone.ru/s3/multimedia-1-g/wc1000/7029633184.jpg',
+        name: 'product2',
+        price: 400
+    },
+    {
+        img: 'https://ir-3.ozone.ru/s3/multimedia-1-8/wc1000/7077535460.jpg',
+        name: 'product3',
+        price: 900
+    },
+    {
+        img: 'https://ir-3.ozone.ru/s3/multimedia-1-7/wc1000/7029624787.jpg',
+        name: 'product4',
+        price: 1000
+    },
+    {
+        img: 'https://ir-3.ozone.ru/s3/multimedia-1-7/wc1000/7029624787.jpg',
+        name: 'product5',
+        price: 1100
+    },
+]
